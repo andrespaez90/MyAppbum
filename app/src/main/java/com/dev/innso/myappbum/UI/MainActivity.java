f06b6a248d0905cc266665ded3eba01d0014b90a
Andrés Páez <andres.paez@innso.co>
2015-07-02 20:44:53 -0500

    Main menu withn animation

--- app/src/main/java/com/dev/innso/myappbum/UI/MainActivity.java
+++ app/src/main/java/com/dev/innso/myappbum/UI/MainActivity.java
@@ -3,7 +3,6 @@ package com.dev.innso.myappbum.UI;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,7 +14,6 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
@@ -68,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         init();
 
         setSupportActionBar(toolbar);
-        View menu = LayoutInflater.from(this).inflate(R.layout.menu_main,null);
+        View menu = LayoutInflater.from(this).inflate(R.layout.fragment_main_menu,null);
         root.addView(menu);
 
         new GuillotineAnimation.GuillotineBuilder(menu, menu.findViewById(R.id.menu_imgmenu), contentHamburger)
